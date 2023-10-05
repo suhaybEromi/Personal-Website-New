e3c1ba99ff026b008f74f4c1eb6ddddf22b4833b
suhaybEromi <108795119+suhaybEromi@users.noreply.github.com>
2023-10-05 11:48:45 +0300

    Add files via upload

--- Portfolio/lib/app.js
+++ Portfolio/lib/app.js
@@ -59,3 +59,32 @@ const writeLoop2 = async () => {
   }
 };
 writeLoop2();
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+const phrases3 = ["I'm FrontEnd Developer"];
+const el3 = document.getElementById("typewriter3");
+let sleepTime3 = 100;
+let curPhraseIndex3 = 0;
+const writeLoop3 = async () => {
+  while (true) {
+    let curWord = phrases3[curPhraseIndex3];
+    for (let i = 0; i < curWord.length; i++) {
+      el3.innerText = curWord.substring(0, i + 1);
+      await sleep(sleepTime3);
+    }
+    await sleep(sleepTime3 * 10);
+    for (let i = curWord.length; i > 0; i--) {
+      el3.innerText = curWord.substring(0, i - 1);
+      await sleep(sleepTime3);
+    }
+    await sleep(sleepTime3 * 5);
+    if (curPhraseIndex3 === phrases3.length - 1) {
+      curPhraseIndex3 = 0;
+    } else {
+      curPhraseIndex3++;
+    }
+  }
+};
+writeLoop3();
